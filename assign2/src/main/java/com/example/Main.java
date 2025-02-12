02a5c6531ee1f5efa4939b4da44d0b0d6583184d
Nikolas <nikolaslazarov@gmail.com>
2025-02-12 13:34:48 -0500

    initial commit

---
+++ assign2/src/main/java/com/example/Main.java
@@ -0,0 +1,34 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle v = new Vehicle();
+        System.out.println(Vehicle.make());
+        System.out.println(v.model());
+        System.out.println(v.speed());
+        v.go();
+        v.stop();
+    }
+
+    public static class Vehicle {
+        
+        public static String make() {
+            return "make";
+        }
+
+        public String model() {
+            return "model";
+        }
+
+        public int speed() {
+            return 0;
+        }
+        
+        public void go() {
+            System.out.println("Going");
+        }
+        public void stop() {
+            System.out.println("Stopping");
+        }
+    }
+}
